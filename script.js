809b6cefd50704c325275c57a25a55c2c99c4e2a
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-07 08:52:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function displayBrowserInfo() {
+            var browserName = navigator.appName;
+            var version = navigator.appVersion;
+
+            var browserInfo = "You are using " + browserName + " version " + version;
+
+            // Display browser information in the 'browser-info' div
+            document.getElementById('browser-info').textContent = browserInfo;
+        }
+displayBrowserInfo();
